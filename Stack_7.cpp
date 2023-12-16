4578e668deb9010684e1af8e830e075994e3aadf
Ваня Витюк <naharamimo@gmail.com>
2023-12-16 17:40:21 +0200

    Добавьте файлы проекта.

---
+++ Stack_7.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') {
+                stk.push(i);
+            }
+            else {
+                stk.pop();
+                if (stk.empty()) {
+                    stk.push(i);
+                }
+                else {
+                    maxLength = max(maxLength, i - stk.top());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    cout << solution.longestValidParentheses("(()") << endl;
+    cout << solution.longestValidParentheses(")()())") << endl;
+    cout << solution.longestValidParentheses("") << endl; 
+
+    return 0;
+}
